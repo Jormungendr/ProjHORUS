04651104da26402531e73a6e2358445adf03cb16
Razer <zitong.tian@forceclouds.com>
2017-12-19 14:21:19 +0800

    update procfile

--- projhorus/__init__.py
+++ projhorus/__init__.py
@@ -68,4 +68,4 @@ def serve_robots_txt():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, threaded=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, threaded=True, port=80)
